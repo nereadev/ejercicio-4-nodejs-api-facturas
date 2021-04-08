876430de845c77c398c56218b6b1a695e1e4f292
Nerea García <nereagarcia.c@gmail.com>
2021-04-08 17:04:14 +0200

    rutas para las solicitudes GET

--- rutas/facturas.js
+++ rutas/facturas.js
@@ -4,10 +4,30 @@ const { facturas } = require("../facturas.json");
 
 const router = express.Router();
 
-router.get("/facturas", (req, res, next) => {
-  res.json(facturas);
+router.get("/", (req, res, next) => {
+  const facturasObjeto = {
+    total: facturas.length,
+    datos: facturas
+  };
+  res.json(facturasObjeto);
 });
-router.get("/facturas/factura/:idFactura", (req, res, next) => {
+router.get("/ingresos", (req, res, next) => {
+  const facturaIngreso = facturas.filter(factura => factura.tipo === "ingreso");
+  const facturasIngresoObjeto = {
+    total: facturaIngreso.length,
+    datos: facturaIngreso
+  };
+  res.json(facturasIngresoObjeto);
+});
+router.get("/gastos", (req, res, next) => {
+  const facturaIngreso = facturas.filter(factura => factura.tipo === "gasto");
+  const facturasIngresoObjeto = {
+    total: facturaIngreso.length,
+    datos: facturaIngreso
+  };
+  res.json(facturasIngresoObjeto);
+});
+router.get("/factura/:idFactura", (req, res, next) => {
   const facturaNumero = +req.params.idFactura;
   const facturaBuscada = facturas.filter(factura => factura.id === facturaNumero);
   res.json(facturaBuscada);
